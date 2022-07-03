5d6478f9c9ad2014c35248c444a12a72c6e91de6
AppleStein <pianonono1129@gmail.com>
2022-07-03 11:17:06 +0900

    制作途中:オブジェクトの停止はさせたがその後動いてくれない

--- Assets/Scripts/ManagePanel.cs
+++ Assets/Scripts/ManagePanel.cs
@@ -8,12 +8,18 @@ public class ManagePanel : MonoBehaviour
 {
     private GameObject judgeObject;
     JudgeGame judgeScript;
+    private GameObject Character;
+    private Rigidbody2D rb;
+
+
     [SerializeField] GameObject GameOverPanel;
     [SerializeField] GameObject TitlePanel;
 
     void Start()
     {
         Debug.Log(1);
+        Character = GameObject.Find("Character");
+        rb = Character.GetComponent<Rigidbody2D>();
         judgeObject = GameObject.Find("JudgeObject");
         judgeScript = judgeObject.GetComponent<JudgeGame>();
     }
@@ -30,6 +36,7 @@ public class ManagePanel : MonoBehaviour
     public void GameStart()
     {
         Debug.Log(4);
+        rb.velocity = Vector3.zero;
         GameOverPanel.SetActive(false);
         TitlePanel.SetActive(false);
     }
